fd885b58fddc445b5c0dfa7a3397168e2032b65e
Mac Stevens <stevensm@earthlink.net>
2013-12-24 09:38:46 -0800

    WIP

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -11,7 +11,303 @@
 #define CF_DEBUG_ASSERT( _condition_ ) {if(!(_condition)) throw -1; }
 #define CF_ASSERT( _condition_ ) assert(_condition_)
 
-typedef unsigned long long int uint64;
+
+typedef void *void_ptr;
+typedef unsigned char boolean;
+typedef char int8;
+typedef unsigned char uint8;
+typedef short int16;
+typedef unsigned short uint16;
+typedef long int32;
+typedef unsigned long uint32;
+typedef long long int64;
+typedef unsigned long long uint64;
+typedef double float64;
+
+
+
+typedef enum
+{
+    CF01_OT_OBJECT = 0,
+    CF01_OT_PROCEDURE,
+
+    CF01_OT_XYZABC,     /* template code */
+
+    CF01_OT_COUNT
+} cf01_managed_object_type;
+
+typedef enum
+{
+    CF01_RPCT_NONE = 0,
+    CF01_RPCT_ARRAY_3,
+    CF01_RPCT_LIST,
+    CF01_RPCT_TREE,
+
+    CF01_RPCT_COUNT
+} cf01_reverse_pointer_container_type;
+
+
+
+
+typedef void *cf01_void_ptr_array3[3];
+
+
+typedef struct cf01_ptr_list_node
+{
+    void *m_ptr;
+    struct cf01_ptr_list_node *m_prev;
+    struct cf01_ptr_list_node *m_next;
+} cf01_ptr_list_node;
+
+typedef struct cf01_ptr_list
+{
+    void *m_cf01_ptr_list_node_allocator;
+    cf01_ptr_list_node *m_head;
+    uint32 m_size;
+} cf01_ptr_list;
+
+void cf01_ptr_list_init(cf01_ptr_list *p);
+void cf01_ptr_list_clear(cf01_ptr_list *p);
+void cf01_ptr_list_push_back(cf01_ptr_list *obj_data, void *p);
+uint64 cf01_ptr_list_verify_data(const cf01_ptr_list *pl, 
+    cf01_str_vec *messages);
+
+
+typedef struct cf01_managed_object_data
+{
+    cf01_managed_object_type m_object_type;
+    cf01_reverse_pointer_container_type m_rev_ptr_ctr_type;
+    void *m_allocator;
+    union
+    {
+        cf01_void_ptr_array3 m_rev_ptr_array_3;
+        cf01_ptr_list m_rev_ptr_list;
+        /* tree */
+    };
+} cf01_managed_object_data;
+
+void cf01_mng_obj_add_rev_ptr(cf01_managed_object_data *obj_data, void *p);
+void cf01_mng_obj_remove_rev_ptr(cf01_managed_object_data *obj_data, void *p);
+boolean cf01_mng_obj_has_rev_ptr(const cf01_managed_object_data *obj_data,
+    const void *p);
+
+void cf01_write_mng_obj(cf01_writer *w, cf01_managed_object_data *obj_data)
+{
+    w->write_open_tag("obj_data");
+    w->write_line_break();
+    w->write_open_tag("obj_type");
+    w->write_string(/*m_object_type to string */);
+    w->write_close_tag("obj_type");
+    w->write_line_break();
+
+    /* ... */
+ 
+    w->write_close_tag("obj_data");
+
+
+}
+
+
+
+
+struct cf01_data_type;
+
+struct cf01_data_struct;
+
+typedef struct cf01_data_struct_element
+{
+    struct cf01_data_type *m_data_type;
+    cf01_string *m_name;
+} cf01_data_struct_element;
+
+typedef struct cf01_data_struct
+{
+    cf01_managed_object_data m_object_data; /* must be first */
+    cf01_data_struct_element *m_data_type_array;
+    uint32 m_data_type_array_length;
+    uint32 m_data_type_array_capacity;
+} cf01_data_struct;
+
+
+
+
+typedef struct cf01_procedure
+{
+    cf01_managed_object_data m_object_data; /* must be first */
+
+    struct cf01_data_struct *m_data_struct;
+
+    /*
+    data
+      data type    name
+      data type    name
+      data type    name
+      pointer type
+        owning pointer
+        non-owning pointer
+        shared ownership pointer
+          automatic garbage collected pointer
+      textual representation
+      image representation
+      debug output procedure[s]
+      text comment[s]
+
+
+
+    main procedure = essential to achieve post conditions
+    sub-optional procedures    = sub-procedures or instructions interlaced with the main instruction sequence -- purpose is to support assertions or debug output
+
+
+    preconditions
+    postconditions
+    intermediate assertions
+    textual representation[s]
+    image representation[s]
+    compiled representation[s]
+    
+    instruction sequence
+      sequence point
+      instruction
+      sequence point
+      instruction
+      sequence point
+      instruction
+      sequence point
+      instruction
+      sequence point
+
+    
+
+    sequence point
+      prev instruction[s]
+      next instruction[s]
+      checks
+
+    
+ 
+    instruction
+      prev sequence point[s]
+      next sequence point[s]
+      parent procedure[s]
+        main parent procedure
+        optional parent procedure
+      implementation procedure
+      map from parent procedure data to implementation procedure data
+      instruction type: assertion, nop, ordinary instruction, branch instruction
+      if assertion
+        required optional procedure
+      link to text representation
+      link to alternate equivalent instruction
+      link to other representation
+
+
+
+
+required procedure = main procedure
+optional procedure = 
+  required to perform assertion
+assertion
+  warning
+    % confidence
+  error
+
+
+
+    */
+    
+} cf01_procedure;
+
+void cf01_proc_init(cf01_procedure *p);
+void cf01_proc_clear(cf01_procedure *p);
+uint64 cf01_proc_verify_data(const cf01_procedure *p, cf01_str_vec *messages);
+
+typedef struct cf01_time
+{
+    /*  base time */
+    /* enum {base_time_epoch, unix_1970_epoch} m_epoch */
+    /* enum { seconds, milliseconds, microseconds, weeks, years } m_time_unit */
+    int64 m_ticks;
+} cf01_time;
+
+
+typedef struct cf01_file_data
+{
+    cf01_managed_object_data m_object_data; /* must be first */
+    cf01_string m_file_name;
+    cf01_str_vec m_relative_file_path;
+    cf01_str_vec m_absolute_file_path;
+    uint64 m_file_length;
+    uint32 m_adler_32_sum;
+    uint8 m_md5_sum[16];
+    cf01_time m_modified_time;
+    uint8 m_is_this_file;
+    
+  
+} cf01_file_data;
+
+
+typedef struct cf01_file_sub_section
+{
+    cf01_managed_object_data m_object_data; /* must be first */
+    cf01_file_data *m_file_data;
+    uint64 m_section_begin_pos;
+    uint64 m_section_end_pos;   
+    cf01_string m_first_bytes;
+    cf01_string m_last_bytes;
+    uint32 m_begin_line_index;
+    uint32 m_end_line_index;
+    uint16 m_begin_line_offset;
+    uint16 m_end_line_offset;
+    uint32 m_adler_32_sum;
+} cf01_file_sub_section;
+
+
+typedef enum
+{
+    CF01_RT_NONE = 0,
+
+    /* attribute (one-way relation, so to speak) */
+
+    /* binary relation */
+    CF01_RT_PREV_NEXT,
+    CF01_RT_BEFORE_AFTER,
+
+    /* ternary relation */
+
+    /* n-way relation */
+
+    CF01_RT_COUNT
+} cf01_relation_type;
+
+
+typedef struct cf01_unary_relation
+{
+    cf01_managed_object_data m_object_data; /* must be first */
+    cf01_relation_type m_relation_type; /* must be second */
+    void *m_object_0;
+} cf01_unary_relation;
+
+typedef struct cf01_binary_relation
+{
+    cf01_managed_object_data m_object_data; /* must be first */
+    cf01_relation_type m_relation_type; /* must be second */
+    void *m_object_0;
+    void *m_object_1;
+} cf01_binary_relation;
+
+
+typedef struct cf01_ternary_relation
+{
+    cf01_managed_object_data m_object_data; /* must be first */
+    cf01_relation_type m_relation_type; /* must be second */
+
+    void *m_object_0;
+    void *m_object_1;
+    void *m_object_2;
+
+} cf01_ternary_relation;
+
+
 
 
 
